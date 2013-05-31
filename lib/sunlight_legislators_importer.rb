951d2891f93d1e81218806359edc6b61476d4b29
kvanderd <me@katharine.org>
2013-05-30 20:28:18 -0700

    change spelling error sung -> sun

--- lib/sunlight_legislators_importer.rb
+++ lib/sunlight_legislators_importer.rb
@@ -1,14 +1,16 @@
 require 'csv'
+require_relative '../app/models/politician'
 
 class SunlightLegislatorsImporter
   def self.import(filename)
+    atrribute_array = Politician.columns.map(&:name)
     csv = CSV.new(File.open(filename), :headers => true)
     csv.each do |row|
+      attributes = Hash.new
       row.each do |field, value|
-        # TODO: begin
-        raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
-        # TODO: end
+         attributes[field.to_sym]= value  if atrribute_array.include?(field)
       end
+      Politician.create!(attributes)
     end
   end
 end
@@ -21,3 +23,5 @@ rescue ArgumentError => e
 rescue NotImplementedError => e
   $stderr.puts "You shouldn't be running this until you've modified it with your implementation!"
 end
+
+SunlightLegislatorsImporter.import("legislators.csv")
